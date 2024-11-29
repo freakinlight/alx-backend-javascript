f5ffb679c3e26a2c0bc851685335c9291aa8802e
freakinlight <mazouznour0@gmail.com>
2024-11-29 11:02:39 +0000

    6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// Create an Express app
+const app = express();
+
+// Define the root route "/"
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+// Listen on port 1245
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+// Export the app
+module.exports = app;
