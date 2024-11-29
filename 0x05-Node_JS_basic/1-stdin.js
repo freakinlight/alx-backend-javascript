26d2711721c6c8360552e07feb4b60142d7de322
freakinlight <mazouznour0@gmail.com>
2024-11-29 08:59:34 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (input) => {
   const name = input.toString().trim(); // Remove extra spaces or newlines
   process.stdout.write(`Your name is: ${name}\n`);
+
+  // Simulate end of input for testing with echo
+  process.exit(0);
 });
 
-process.stdin.on('end', () => {
+// Handle actual EOF (Ctrl+D)
+process.on('exit', () => {
   console.log('This important software is now closing');
 });
